992c787be4a921691546519852926693084ab92f
LorcanS1993 <G00289865@atu.ie>
2022-11-03 13:01:33 +0000

    Added new code in Passenger and PassengerTest.java

--- src/test/java/ie/atu/Passenger/PassengerTest.java
+++ src/test/java/ie/atu/Passenger/PassengerTest.java
@@ -2,16 +2,46 @@ package ie.atu.Passenger;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+    public class PassengerTest {
+        Passenger title;
+        Passenger age;
+        Passenger phone;
+        Passenger name;
+        Passenger id;
 
-class PassengerTest {
+        @BeforeEach
+        void setUp() {
+            title = new Passenger();
+            age = new Passenger();
+            phone = new Passenger();
+            name = new Passenger();
+            id = new Passenger();
+        }
 
-    @BeforeEach
-    void setUp() {
+
+        @Test
+        void testTitle(){
+            assertEquals("Mr", title.title("Mr"));
+        }
+
+        @Test
+        void testAge(){
+            assertEquals("29", age.age("29"));
+        }
+
+        @Test
+        void testPhone(){
+            assertEquals("0851234567", phone.phone("0851234567"));
+        }
+        @Test
+        void testPhone(){
+            assertEquals("0851234567", phone.phone(""));
+        }
+
+        @AfterEach
+        void tearDown() {
+        }
     }
-
-    @AfterEach
-    void tearDown() {
-    }
-}
